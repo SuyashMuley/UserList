0635060d5b05fb3e77d5b6f3d7c640ac03ecbcde
SuyashMuley <59868486+SuyashMuley@users.noreply.github.com>
2021-12-26 19:15:24 +0530

    initial Commit

---
+++ search.js
@@ -0,0 +1,29 @@
+// Js for Fetching data and searching through users
+var app = new Vue({
+    el: "#app",
+    data: {
+      users: [],
+      filter: '',
+
+    },
+    // Fetch from json
+    mounted: function () {
+        fetch("/sample_user_data_20k.json")
+        .then(response=> response.json())
+        .then(userList => {
+            this.users = userList.objects;
+            console.log(typeof(userList));
+        },
+        )
+    },
+    // Computed function for filtering through users
+    computed: {
+        filtered(){
+            if(this.filter){
+                return this.users.filter((item)=>{
+                    return this.filter.toLowerCase().split(' ').every(v=> item.FirstNameLastName.toLowerCase().includes(v))  
+                })
+            }
+        }
+    }
+  });
